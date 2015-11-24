5e48ac637507192efd564dd968829aa5fd8334dd
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-24 23:50:29 +0000

    Koan about time

--- about_time.pl
+++ about_time.pl
@@ -2,20 +2,20 @@
 =pod
     Copyright (c) 2012, Salesforce.com, Inc.
     All rights reserved.
-    
+
     Redistribution and use in source and binary forms, with or without modification,
     are permitted provided that the following conditions are met:
-    
+
     Redistributions of source code must retain the above copyright notice, this list
     of conditions and the following disclaimer. Redistributions in binary form must
     reproduce the above copyright notice, this list of conditions and the following
     disclaimer in the documentation and/or other materials provided with the
     distribution.
-    
+
     Neither the name of salesforce.com, inc. nor the names of its contributors may be
     used to endorse or promote products derived from this software without specific
     prior written permission.
-    
+
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -39,52 +39,53 @@ use Perl::Koans;
 
 sub about_time {
     # about_time()
-    
+
     my $before = time();
     sleep 1;
     my $after = time();
-    
-    is ($before > $after, 1, "time() returns the number of seconds since the epoch, 00:00 January 1, 1970 GMT");
-    
+
+    is ($before < $after, 1, "time() returns the number of seconds since the epoch, 00:00 January 1, 1970 GMT");
+
     # more information in about_context.pl
     my @lt_array  = localtime(); # definition of elements: http://perldoc.perl.org/functions/localtime.html
     my $lt_scalar = localtime();
-    
-    is   ($lt_array[5], __ - 1900, "index 5 is the current year less 1900"); # TODO figure out how we can test the other elements in a dynamic, but not scary way
-    like ($lt_scalar, qr/__/, "in scalar context, localtime() gives us a human readable timestamp"); # TODO is this hint clear/vague enough? i want you to match anything you know will be in the timestamp: day of week, month, day of month    
-    
+
+    is   ($lt_array[5], 2015 - 1900, "index 5 is the current year less 1900"); # TODO figure out how we can test the other elements in a dynamic, but not scary way
+    like ($lt_scalar, qr/^\w\w\w \w\w\w \d\d \d\d:\d\d:\d\d \d\d\d\d$/, "in scalar context, localtime() gives us a human readable timestamp"); # TODO is this hint clear/vague enough? i want you to match anything you know will be in the timestamp: day of week, month, day of month
+
     # gmtime is just like localtime(), except it returns values localized to Greenwich
     my @gmt_array  = gmtime();
     my $gmt_scalar = gmtime();
-    
-    is ($#lt_array, __, "gmttime() has the same element count as localtime()");
-    isnt (__, \@gmt_array, "gmttime() has different contents than localtime()"); # TODO better hint
-    
+
+    is ($#lt_array, $#gmt_array, "gmttime() has the same element count as localtime()");
+    isnt (\@lt_array, \@gmt_array, "gmttime() has different contents than localtime()"); # TODO better hint
+
     ## TODO: add a gmttime($lt_scalar) and localtime($gmt_scalar) type test to show how it can be used as a function as well
-    
-    my $start = localtime();
-    sleep 1; # TODO yuck. should we just reuse $lt_scalar?
-    my $finish = localtime();
-    
-    is ($finish - $start, __, "localtime() is often used to keep track of how long an operation takes"); # TODO yeah, this is not a good hint. its true, but you should be using time() instead
-    
-    
+
+    # This koan is broken, return value is not seconds
+    # my $start = localtime();
+    # sleep 1; # TODO yuck. should we just reuse $lt_scalar?
+    # my $finish = localtime();
+
+    # is ($finish - $start, 0, "localtime() is often used to keep track of how long an operation takes"); # TODO yeah, this is not a good hint. its true, but you should be using time() instead
+
+
     return (Perl::Koans::get_return_code());
-    
+
 }
 
 sub about_time_hi_res {
     # about_time_hi_res()
-    
+
     # Time::HiRes is in core as of 5.8, but if you don't have it, use CPAN to install
- 
+
     use Time::HiRes; # in production code, all modules that are 'use'd should be grouped at the beginning of your script, only here for clarity
-    
+
     my $start  = Time::HiRes::gettimeofday();
     my $finish = Time::HiRes::gettimeofday();
-    
-    is ($finish - $start > __, 1, "Time::HiRes::gettimeofday() gives you microsecond precision");
-    
+
+    is ($finish - $start > 0, 1, "Time::HiRes::gettimeofday() gives you microsecond precision");
+
     return (Perl::Koans::get_return_code());
 }
 
@@ -99,10 +100,10 @@ unless (caller(0)) {
 sub run {
     # run() -- runs all functions in this module
     my $results = 0;
-    
+
     $results += about_time();
     $results += about_time_hi_res();
-    
+
     return ($results) ? bail($results) : $results;
 }
 
